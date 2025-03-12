5aabecd66aabc5644f59dee14c9382a5a9883629
FunnyRakoon <hiwa.pouf049@gmail.com>
2025-03-12 14:20:43 +0100

    deply

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://FunnyRakoon.github.io',
+  base: 'Td-astro',
   vite: {
     plugins: [tailwindcss()]
   }
